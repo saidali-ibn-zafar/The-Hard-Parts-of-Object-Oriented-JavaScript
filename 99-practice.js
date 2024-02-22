7993978a69934ae9db11385fa5010c7686697cdd
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-02-22 15:29:30 -0700

    Create 99-practice.js

---
+++ 99-practice.js
@@ -0,0 +1,165 @@
+/****************************************************************
+                  WORKING WITH OBJECT LITERALS
+****************************************************************/
+
+/*** CHALLENGE 1 ***/
+
+function makePerson(name, age) {
+	const newUser = {};
+  newUser.name = name;
+  newUser.age = age;
+  return newUser;
+
+
+}
+
+const vicky = makePerson('Vicky', 24);
+
+
+// /********* Uncomment these lines to test your work! *********/
+// console.log(vicky.name); // -> Logs 'Vicky'
+// console.log(vicky.age); // -> Logs 24
+
+
+
+
+
+/****************************************************************
+                       USING OBJECT.CREATE
+****************************************************************/
+
+/*** CHALLENGE 2 ***/
+
+const personStore = Object.create(null);
+
+personStore.greet = function() {
+console.log("hello")
+}
+
+// /********* Uncomment this line to test your work! *********/
+// personStore.greet(); // -> Logs 'hello'
+
+
+
+/*** CHALLENGE 3 ***/
+
+function personFromPersonStore(name, age) {
+	const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+  return person;
+
+
+}
+
+const sandra = personFromPersonStore('Sandra', 26);
+
+
+// /********* Uncomment these lines to test your work! *********/
+// console.log(sandra.name); // -> Logs 'Sandra'
+// console.log(sandra.age); //-> Logs 26
+// sandra.greet(); //-> Logs 'hello'
+
+
+
+/*** CHALLENGE 4 ***/
+
+personStore.introduce = function(){
+	console.log(`Hi, my name is ${this.name}`)
+}
+
+// sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+
+
+
+
+
+/****************************************************************
+                    USING THE 'NEW' KEYWORD
+****************************************************************/
+
+/*** CHALLENGE 5 ***/
+
+function PersonConstructor() {
+	this.greet = function (){console.log('hello')}
+
+}
+
+
+// /********* Uncomment this line to test your work! *********/
+const simon = new PersonConstructor;
+// simon.greet(); // -> Logs 'hello'
+
+
+
+/*** CHALLENGE 6 ***/
+
+function personFromConstructor(name, age) {
+	this.name = name;
+  this.age = age;
+  this.greet = ()=>{console.log('hello')}
+
+
+}
+
+const mike = new personFromConstructor('Mike', 30);
+
+
+// /********* Uncomment these lines to test your work! *********/
+// console.log(mike.name); // -> Logs 'Mike'
+// console.log(mike.age); //-> Logs 30
+// mike.greet(); //-> Logs 'hello'
+
+
+
+/*** CHALLENGE 7 ***/
+personFromConstructor.prototype.introduce = function () {console.log(`Hi, my name is ${this.name}`)}
+
+
+// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+
+/****************************************************************
+                        USING ES6 CLASSES
+****************************************************************/
+
+/*** CHALLENGE 8 ***/
+
+class PersonClass {
+	constructor() {
+    // add code here
+
+
+	}
+
+	greet = function () {console.log('hello')}
+
+}
+
+
+// /********* Uncomment this line to test your work! *********/
+const george = new PersonClass;
+// george.greet(); // -> Logs 'hello'
+
+
+
+/*** CHALLENGE 9 ***/
+
+class DeveloperClass{
+
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+  
+  introduce = function(){
+    console.log(`Hello World, my name is ${this.name}`)
+  }
+}
+
+
+// /********* Uncomment these lines to test your work! *********/
+// const thai = new DeveloperClass('Thai', 32);
+// console.log(thai.name); // -> Logs 'Thai'
+// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
+
